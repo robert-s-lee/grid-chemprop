80c869546e30e3322b8b7de98208991eab44ac27
Robert S Lee <sangkyulee@gmail.com>
2021-08-26 15:05:38 -0400

    adjust datastore

--- run.sh
+++ run.sh
@@ -1,8 +1,3 @@
 #!/usr/bin/env bash
 
-echo df -kh
-df -kh
-echo du -sh $2/*
-df -kh
-
-chemprop_train --data_path "$2" --dataset_type classification --save_dir tox21_checkpoints
+chemprop_train --data_path "$2"/tox21.csv --dataset_type classification --save_dir tox21_checkpoints
