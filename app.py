5a2f4d1b6c2b17b2dbc35f6bf66a79af5ce74e23
Robert S Lee <sangkyulee@gmail.com>
2022-06-26 14:31:03 -0400

    add sudo apt-get install -y ffmpeg libsm6 libxext6 for mathplotlib

--- app.py
+++ app.py
@@ -5,9 +5,12 @@ from scripts.bashwork import LitBashWork
 class ChempropBuildConfig(L.BuildConfig):
   def build_commands(self) -> List[str]:
       return [
-          "pip install rdkit-pypi",
-          "pip install git+https://github.com/bp-kelley/descriptastorus",
-          "pip install chemprop",
+          "cat /proc/sys/kernel/shmmax",
+          "sudo apt-get update",
+          "sudo apt-get install -y ffmpeg libsm6 libxext6",          
+          "python -m pip install rdkit",
+          "python -m pip install git+https://github.com/bp-kelley/descriptastorus",
+          "python -m pip install -e chemprop",
       ]
 
 class LitFlow(L.LightningFlow):
@@ -19,10 +22,10 @@ class LitFlow(L.LightningFlow):
       )
   def run(self):
     #self.my_work.run("tensorboard --logdir lightning_logs --host ${host} --port ${port}")
-    self.my_work.run("wget -q https://github.com/chemprop/chemprop/raw/master/data.tar.gz")
-    self.my_work.run("tar -xvzf data.tar.gz")
-    self.my_work.run(f"chemprop_web  --host={self.my_work.host} --port={self.my_work.port}", wait_for_exit=False)
-    self.my_work.run("chemprop_train --data_path data/tox21.csv --dataset_type classification --save_dir tox21_checkpoints")
+    self.my_work.run("wget -q https://github.com/chemprop/chemprop/raw/master/data.tar.gz", cwd="chemprop")
+    self.my_work.run("tar -xvzf data.tar.gz", cwd="chemprop")
+    self.my_work.run(f"python web.py  --host={self.my_work.host} --port={self.my_work.port}", wait_for_exit=False, cwd="chemprop")
+    self.my_work.run("python train.py --data_path data/tox21.csv --dataset_type classification --save_dir tox21_checkpoints", cwd="chemprop")
 
   def configure_layout(self):
     tab1 = {"name": "Training Diag", "content":self.my_work}
